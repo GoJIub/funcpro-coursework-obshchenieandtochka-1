a463ae5dc8787202fd95be64f5fa2df29132c79c
Zhenia <zhenia.saharow@yandex.ru>
2026-05-02 16:43:19 +0300

    test: split Tests.fs into per-module test files

---
+++ tests/Language.Tests/BuiltinTests.fs
@@ -0,0 +1,6 @@
+module Language.Tests.BuiltinTests
+
+open Language
+open Xunit
+
+// Тесты будут добавлены после реализации встроенных функций (Участник 3)
---
+++ tests/Language.Tests/ExampleTests.fs
@@ -0,0 +1,6 @@
+module Language.Tests.ExampleTests
+
+open Language
+open Xunit
+
+// Тесты будут добавлены после реализации парсера (Участник 2)
